935e27edad80ab64055be172821f59ee1f81460d
Kristine Orpilla <143130393+kristineorpilla03@users.noreply.github.com>
2023-11-14 21:41:03 -1000

    Finished Add Event Page

---
+++ app/imports/api/event/Event.js
@@ -0,0 +1,33 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+/**
+ * The EventsCollection. It encapsulates state and variable values for event.
+ */
+class EventsCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'EventsCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      name: String,
+      location: String,
+      description: String,
+      dateTime: Date,
+      image: String,
+    });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+/**
+ * The singleton instance of the StuffsCollection.
+ * @type {EventsCollection}
+ */
+export const Events = new EventsCollection();
