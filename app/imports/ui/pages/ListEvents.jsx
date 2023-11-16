3d1809011d64c110219785f3f2399acdda2f55b9
sierranmorales <143547246+sierranmorales@users.noreply.github.com>
2023-11-15 19:30:59 -1000

    Added Events Page
    Added ListEvents.jsx

---
+++ app/imports/ui/pages/ListEvents.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Col, Container, Row } from 'react-bootstrap';
+import { useTracker } from 'meteor/react-meteor-data';
+import LoadingSpinner from '../components/LoadingSpinner';
+import EventCard from '../components/EventCard';
+import { Events } from '../../api/event/Event';
+
+/* Renders a table containing all of the Profile documents. Use <StuffItem> to render each row. */
+const ListEvents = () => {
+  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+  const { ready, events } = useTracker(() => {
+    // Note that this subscription will get cleaned up
+    // when your component is unmounted or deps change.
+    // Get access to Profile documents.
+    const subscription = Meteor.subscribe(Events.userPublicationName);
+    // Determine if the subscription is ready
+    const rdy = subscription.ready();
+    // Get the Event documents
+    const eventItems = Events.collection.find({}).fetch();
+    return {
+      events: eventItems,
+      ready: rdy,
+    };
+  }, []);
+
+  return (ready ? (
+    <Container className="py-3">
+      <Row className="justify-content-center">
+        <Col>
+          <Col className="text-center">
+            <h2>Explore Events</h2>
+          </Col>
+          <Row xs={1} md={2} lg={3} className="g-4">
+            {events.map((event) => (<Col key={event._id}><EventCard event={event} /></Col>))}
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  ) : <LoadingSpinner />);
+};
+
+export default ListEvents;
