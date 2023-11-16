f2dc83c43c83e862da253ffe0b17b6599199e83e
Harvey Lafradez <97498921+harv-lafradez@users.noreply.github.com>
2023-11-15 18:29:40 -1000

    removed background image on landing
    also added the kahakō in the text

--- app/imports/ui/components/About.jsx
+++ app/imports/ui/components/About.jsx
@@ -4,10 +4,10 @@ import { Container, Row } from 'react-bootstrap';
 const About = () => (
   <Container>
     <Row className="align-middle text-center py-5">
-      <h1>Welcome to Manoa Melody!</h1>
-      <h3>Manoa Melody aims to connect University of Hawaii at Manoa students through music!</h3>
+      <h1>Welcome to Mānoa Melody!</h1>
+      <h3>Mānoa Melody aims to connect University of Hawaii at Mānoa students through music!</h3>
       <h3>Music is a powerful tool that allows people to express themselves freely while helping them share their personal experiences.</h3>
-      <h3>Users can choose what instruments they are interested in or music genres they enjoy and are matched with UH Manoa students who share the same interests.</h3>
+      <h3>Users can choose what instruments they are interested in or music genres they enjoy and are matched with UH Mānoa students who share the same interests.</h3>
       <h3>Upload and explore musical events hosted by students and the university and meet new people!</h3>
     </Row>
   </Container>
