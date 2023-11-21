80eb5f6124c6949c9c2774fb96c99c1fc1715ddb
destinyshishido <itsmedest2003@gmail.com>
2023-11-20 21:07:13 -1000

    fixed profile card, added genre + instruments

--- app/imports/ui/components/ProfileCard.jsx
+++ app/imports/ui/components/ProfileCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Image } from 'react-bootstrap';
+import { Card, Image, ListGroup } from 'react-bootstrap';
 
 /** Renders ProfileCard Card * */
 const ProfileCard = ({ profile }) => (
@@ -8,7 +8,14 @@ const ProfileCard = ({ profile }) => (
     <Card.Header className="text-center">
       <Image src={profile.image} width={200} />
       <Card.Title className="mt-2">{profile.displayName}</Card.Title>
-      <Card.Subtitle>Insert interests here :P</Card.Subtitle>
+      <Card.Subtitle>
+        <ListGroup horizontal className="justify-content-center">
+          {profile.genres.map((genre) => (<ListGroup.Item key={genre}>{genre}</ListGroup.Item>))}
+        </ListGroup>
+        <ListGroup horizontal className="justify-content-center">
+          {profile.instruments.map((instrument) => (<ListGroup.Item key={instrument}>{instrument}</ListGroup.Item>))}
+        </ListGroup>
+      </Card.Subtitle>
     </Card.Header>
     <Card.Body>
       <Card.Text>{profile.bio}</Card.Text>
@@ -19,9 +26,10 @@ const ProfileCard = ({ profile }) => (
 // Require a document to be passed to this component.
 ProfileCard.propTypes = {
   profile: PropTypes.shape({
-    displayName: PropTypes.string,
     image: PropTypes.string,
+    displayName: PropTypes.string,
     bio: PropTypes.string,
+    owner: PropTypes.string,
     genres: PropTypes.arrayOf(PropTypes.oneOf(['Rock', 'Pop Music', 'Hip Hop', 'Electronic', 'Jazz', 'Country', 'Alternative', 'Indie', 'Punk Rock', 'Kpop'])),
     instruments: PropTypes.arrayOf(PropTypes.oneOf(['Guitar', 'Piano', 'Violin', 'Flute', 'Saxophone', 'Clarinet', 'Trumpet', 'Cello', 'Bass Guitar', 'Drums'])),
   }).isRequired,
