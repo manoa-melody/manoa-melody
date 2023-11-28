f966833e7817a3a28af65d9b9080352a434a1617
Kristine Orpilla <143130393+kristineorpilla03@users.noreply.github.com>
2023-11-28 10:33:21 -1000

    Fixed Errors

--- app/imports/ui/components/EventCard.jsx
+++ app/imports/ui/components/EventCard.jsx
@@ -3,31 +3,28 @@ import PropTypes from 'prop-types';
 import { Card, Image, ListGroup } from 'react-bootstrap';
 
 /** Renders EventCard Card * */
-const EventCard = ({ event }) => {
-
-  return (
-    <Card className="h-100">
-      <Card.Header className="text-center">
-        <Image src={event.image} width={200}/>
-        <Card.Title className="mt-2">{event.name}</Card.Title>
-        <Card.Subtitle>
-          {event.location}
-          <br/>
-          {event.dateTime.toDateString('en-US')} at {event.dateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', timeZone: 'UTC' })}
-          <ListGroup horizontal className="justify-content-center align-content-center pb-1">
-            {event.genres.map((genre) => (<ListGroup.Item key={genre}>{genre}</ListGroup.Item>))}
-          </ListGroup>
-          <ListGroup horizontal className="justify-content-center align-content-center">
-            {event.instruments.map((instrument) => (<ListGroup.Item key={instrument}>{instrument}</ListGroup.Item>))}
-          </ListGroup>
-        </Card.Subtitle>
-      </Card.Header>
-      <Card.Body>
-        <Card.Text>{event.description}</Card.Text>
-      </Card.Body>
-    </Card>
-  );
-};
+const EventCard = ({ event }) => (
+  <Card className="h-100">
+    <Card.Header className="text-center">
+      <Image src={event.image} width={200} />
+      <Card.Title className="mt-2">{event.name}</Card.Title>
+      <Card.Subtitle>
+        {event.location}
+        <br />
+        {event.dateTime.toDateString('en-US')} at {event.dateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', timeZone: 'UTC' })}
+        <ListGroup horizontal className="justify-content-center align-content-center pb-1">
+          {event.genres.map((genre) => (<ListGroup.Item key={genre}>{genre}</ListGroup.Item>))}
+        </ListGroup>
+        <ListGroup horizontal className="justify-content-center align-content-center">
+          {event.instruments.map((instrument) => (<ListGroup.Item key={instrument}>{instrument}</ListGroup.Item>))}
+        </ListGroup>
+      </Card.Subtitle>
+    </Card.Header>
+    <Card.Body>
+      <Card.Text>{event.description}</Card.Text>
+    </Card.Body>
+  </Card>
+);
 
 // Require a document to be passed to this component.
 EventCard.propTypes = {
