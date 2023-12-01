f964b7f386c112a95df385388afbd999cb2230b0
sierranmorales <143547246+sierranmorales@users.noreply.github.com>
2023-11-30 18:03:52 -1000

    Implement Admin Delete Profiles
    Implemented  profile delete feature on admin profile page

---
+++ app/imports/startup/server/Methods.js
@@ -0,0 +1,17 @@
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import { Profiles } from '../../api/profile/Profiles';
+
+Meteor.methods({
+  'profiles.delete'(_id) {
+    check(_id, String);
+    if (!this.userId) {
+      throw new Meteor.Error('Not Authorized', 'You are not authorized to perform this action');
+    }
+
+    // Optional: Additional checks, e.g., if the user owns the profile or if the user is an admin
+
+    // Perform the deletion
+    return Profiles.collection.remove({ _id });
+  },
+});
--- app/imports/ui/components/ProfileCardAdmin.jsx
+++ app/imports/ui/components/ProfileCardAdmin.jsx
@@ -1,32 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Image, ListGroup } from 'react-bootstrap';
+import swal from 'sweetalert';
+import { Meteor } from 'meteor/meteor';
+import { Card, Image, ListGroup, Button } from 'react-bootstrap';
 
 /** Renders ProfileCard Card * */
-const ProfileCardAdmin = ({ profile }) => (
-  <Card className="h-100">
-    <Card.Header className="text-center">
-      <Image src={profile.image} width={200} />
-      <Card.Title className="mt-2">{profile.displayName}</Card.Title>
-      <Card.Subtitle>
-        <ListGroup horizontal className="justify-content-center align-content-center pb-1">
-          {profile.genres.map((genre) => (<ListGroup.Item key={genre}>{genre}</ListGroup.Item>))}
-        </ListGroup>
-        <ListGroup horizontal className="justify-content-center align-content-center">
-          {profile.instruments.map((instrument) => (<ListGroup.Item key={instrument}>{instrument}</ListGroup.Item>))}
-        </ListGroup>
-      </Card.Subtitle>
-    </Card.Header>
-    <Card.Body>
-      <Card.Text>{profile.bio}</Card.Text>
-      <footer className="blockquote-footer">{profile.owner}</footer>
-    </Card.Body>
-  </Card>
-);
+const ProfileCardAdmin = ({ profile }) => {
+  const handleDelete = () => {
+    swal({
+      title: 'Are you sure?',
+      text: 'Once deleted, you will not be able to recover this profile!',
+      icon: 'warning',
+      buttons: true,
+      dangerMode: true,
+    })
+      .then((willDelete) => {
+        if (willDelete) {
+          Meteor.call('profiles.delete', profile._id, (error) => {
+            if (error) {
+              swal('Error', error.message, 'error');
+            } else {
+              swal('Profile deleted!', {
+                icon: 'success',
+              });
+              // Here, you might want to implement additional logic
+              // like redirecting the user or updating the parent component's state
+            }
+          });
+        }
+      });
+  };
+
+  return (
+    <Card className="h-100">
+      <Card.Header className="text-center">
+        <Image src={profile.image} width={200} />
+        <Card.Title className="mt-2">{profile.displayName}</Card.Title>
+        <Card.Subtitle>
+          <ListGroup horizontal className="justify-content-center align-content-center pb-1">
+            {profile.genres.map((genre) => (<ListGroup.Item key={genre}>{genre}</ListGroup.Item>))}
+          </ListGroup>
+          <ListGroup horizontal className="justify-content-center align-content-center">
+            {profile.instruments.map((instrument) => (<ListGroup.Item key={instrument}>{instrument}</ListGroup.Item>))}
+          </ListGroup>
+        </Card.Subtitle>
+      </Card.Header>
+      <Card.Body>
+        <Card.Text>{profile.bio}</Card.Text>
+        <Button variant="danger" onClick={handleDelete}>Delete Profile</Button>
+      </Card.Body>
+    </Card>
+  );
+};
 
 // Require a document to be passed to this component.
 ProfileCardAdmin.propTypes = {
   profile: PropTypes.shape({
+    _id: PropTypes.string,
     image: PropTypes.string,
     displayName: PropTypes.string,
     bio: PropTypes.string,
