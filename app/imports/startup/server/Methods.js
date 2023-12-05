213a93bca26e4a4272a629dc27b0bc5629f53047
Kristine Orpilla <143130393+kristineorpilla03@users.noreply.github.com>
2023-12-04 15:47:16 -1000

    Admin delete

--- app/imports/startup/server/Methods.js
+++ app/imports/startup/server/Methods.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import { Profiles } from '../../api/profile/Profiles';
+import { Events } from '../../api/event/Event';
 
 Meteor.methods({
   'profiles.delete'(_id) {
@@ -13,5 +14,17 @@ Meteor.methods({
 
     // Perform the deletion
     return Profiles.collection.remove({ _id });
+  },
+
+  'event.delete'(_id) {
+    check(_id, String);
+    if (!this.userId) {
+      throw new Meteor.Error('Not Authorized', 'You are not authorized to perform this action');
+    }
+
+    // Optional: Additional checks, e.g., if the user owns the profile or if the user is an admin
+
+    // Perform the deletion
+    return Events.collection.remove({ _id });
   },
 });
--- app/imports/ui/components/EventCardAdmin.jsx
+++ app/imports/ui/components/EventCardAdmin.jsx
@@ -1,35 +1,66 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Image, ListGroup } from 'react-bootstrap';
+import { Card, Image, ListGroup, Button } from 'react-bootstrap';
+import swal from 'sweetalert';
+import { Meteor } from 'meteor/meteor';
 
 /** Renders EventCardAdmin Card * */
-const EventCardAdmin = ({ event }) => (
-  <Card className="h-100">
-    <Card.Header className="text-center">
-      <Image src={event.image} width={200} />
-      <Card.Title className="mt-2">{event.name}</Card.Title>
-      <Card.Subtitle>
-        {event.location}
-        <br />
-        {event.dateTime.toDateString('en-US')} at {event.dateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', timeZone: 'UTC' })}
-        <ListGroup horizontal className="justify-content-center align-content-center pb-1">
-          {event.genres.map((genre) => (<ListGroup.Item key={genre}>{genre}</ListGroup.Item>))}
-        </ListGroup>
-        <ListGroup horizontal className="justify-content-center align-content-center">
-          {event.instruments.map((instrument) => (<ListGroup.Item key={instrument}>{instrument}</ListGroup.Item>))}
-        </ListGroup>
-      </Card.Subtitle>
-    </Card.Header>
-    <Card.Body>
-      <Card.Text>{event.description}</Card.Text>
-      <footer className="blockquote-footer">{event.owner}</footer>
-    </Card.Body>
-  </Card>
-);
+const EventCardAdmin = ({ event }) => {
+  const handleDelete = () => {
+    swal({
+      title: 'Are you sure?',
+      text: 'Once deleted, you will not be able to recover this event!',
+      icon: 'warning',
+      buttons: true,
+      dangerMode: true,
+    })
+      .then((willDelete) => {
+        if (willDelete) {
+          Meteor.call('event.delete', event._id, (error) => {
+            if (error) {
+              swal('Error', error.message, 'error');
+            } else {
+              swal('Event deleted!', {
+                icon: 'success',
+              });
+              // Here, you might want to implement additional logic
+              // like redirecting the user or updating the parent component's state
+            }
+          });
+        }
+      });
+  };
+
+  return (
+    <Card className="h-100">
+      <Card.Header className="text-center">
+        <Image src={event.image} width={200} />
+        <Card.Title className="mt-2">{event.name}</Card.Title>
+        <Card.Subtitle>
+          {event.location}
+          <br />
+          {event.dateTime.toDateString('en-US')} at {event.dateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', timeZone: 'UTC' })}
+          <ListGroup horizontal className="justify-content-center align-content-center pb-1">
+            {event.genres.map((genre) => (<ListGroup.Item key={genre}>{genre}</ListGroup.Item>))}
+          </ListGroup>
+          <ListGroup horizontal className="justify-content-center align-content-center">
+            {event.instruments.map((instrument) => (<ListGroup.Item key={instrument}>{instrument}</ListGroup.Item>))}
+          </ListGroup>
+        </Card.Subtitle>
+      </Card.Header>
+      <Card.Body>
+        <Card.Text>{event.description}</Card.Text>
+        <footer className="blockquote-footer">{event.owner}</footer>
+        <Button variant="danger" onClick={handleDelete}>Delete Event</Button>
+      </Card.Body>
+    </Card>
+  );
+};
 
 // Require a document to be passed to this component.
 EventCardAdmin.propTypes = {
   event: PropTypes.shape({
+    _id: PropTypes.string,
     name: PropTypes.string,
     location: PropTypes.string,
     description: PropTypes.string,
